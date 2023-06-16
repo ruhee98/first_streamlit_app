ef90206a8838e15b02540724ca0a2c8018307a28
Ruhee S <51857607+ruhee98@users.noreply.github.com>
2023-06-16 17:23:38 -0400

    Update streamlit_app.py
    added header
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamit.text('Hard Boiled Free-Range Egg')
 
